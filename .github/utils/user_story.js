bd91287c06ded63f350f4503812065b22a8f00e4
David Karrick <48275422+Th3Whit3Wolf@users.noreply.github.com>
2022-04-19 22:48:43 -0600

    Create user_story.js

---
+++ .github/utils/user_story.js
@@ -0,0 +1,170 @@
+const { Octokit } = require("@octokit/action");
+
+const octokit = new Octokit();
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const eventPayload = require(process.env.GITHUB_EVENT_PATH);
+
+console.log({eventPayload})
+
+const issue_number = eventPayload.issue.number;
+const title = `As a ${ISSUE_DATA.person}, I want to ${ISSUE_DATA.goal}, so that ${ISSUE_DATA.goal}`;
+
+const parseIssuePayload = payload => {
+    let headerCount = 0;
+    let issueObj = {};
+
+    payload.issue.body.split("\n\n").forEach(str => {
+        switch (str) {
+            case "### As a ...":
+                headerCount = 1;
+                break;
+            case "### I want to ...":
+                headerCount = 2;
+                break;
+            case "### So that ...":
+                headerCount = 3;
+                break;
+            case "### Where does this user story require work done?":
+                headerCount = 4;
+                break;
+            case "### Which project does this user story belong?":
+                headerCount = 5;
+                break;
+            case "### Acceptance criteria":
+                headerCount = 6;
+                break;
+            case "### Resources":
+                headerCount = 7;
+                break;
+            case "### Notes":
+                headerCount = 8;
+                break;
+            default:
+                switch (headerCount) {
+                    case 1:
+                        issueObj["persona"] = str;
+                        break;
+                    case 2:
+                        issueObj["goal"] = str;
+                        break;
+                    case 3:
+                        issueObj["reason"] = str;
+                        break;
+                    case 4:
+                        issueObj["labor"] = str;
+                        break;
+                    case 5:
+                        issueObj["project"] = str;
+                        break;
+                    case 6:
+                        if (typeof issueObj["criteria"] === 'undefined') {
+                            issueObj["criteria"] = [];
+                        }
+                        if (str !== "_No response_") {
+                            issueObj["criteria"].push(str);
+                        }
+
+                        break;
+                    case 7:
+                        if (typeof issueObj["resources"] === 'undefined') {
+                            issueObj["resources"] = [];
+                        }
+                        if (str !== "_No response_") {
+                            issueObj["resources"].push(str);
+                        }
+                        break;
+                    case 8:
+                        if (typeof issueObj["notes"] === 'undefined') {
+                            issueObj["notes"] = [];
+                        }
+                        if (str !== "_No response_") {
+                            issueObj["notes"].push(str);
+                        }
+                        break;
+                }
+                break;
+        }
+    })
+
+    if (typeof issueObj["labels"] === 'undefined') {
+        issueObj["labels"] = [];
+    }
+
+    switch (issueObj.labor) {
+        case "Backend":
+            issueObj.labels.push("Labor: Backend");
+            break;
+        case "Frontend":
+            issueObj.labels.push("Labor: Frontend");
+            break;
+        case "Both":
+            issueObj.labels.push("Labor: Full Stack");
+            break;
+    }
+
+    switch (issueObj.project) {
+        case "MVP":
+            issueObj.labels.push("Project: MVP");
+            break;
+    }
+
+    return issueObj;
+}
+
+const mkNewBody = (issue, newTasks) => {
+    const { persona, goal, reason, criteria, resources, notes } = issue;
+    let mdArr = [];
+    if (newTasks?.length > 0) {
+        mdArr.push(`### Acceptance Criteria\n${newTasks.join("\n")}`)
+    } else if (resources.length > 0) {
+        mdArr.push(`### Acceptance Criteria\n${criteria.join("\n")}`)
+    }
+    if (resources.length > 0) {
+        mdArr.push(`### Resources\n${resources.join("\n")}`)
+    }
+    if (notes.length > 0) {
+        mdArr.push(`### Notes\n${notes.join("\n")}`)
+    }
+    const md = (mdArr.length > 0) ? `\n\n${mdArr.join("\n\n")}` : "";
+    return `## User Story
+
+    As a ${persona}, I want to ${goal}, so that ${reason}.${md}`
+}
+
+const mkTaskIssues = async (tasks) => {
+    let newTasks = [];
+    tasks.forEach(task => {
+        task = task.replace("- [ ]", "");
+        task = task.trim();
+        const title = task[0].toUpperCase() + task.substring(1).replaceAll("_", " ");
+
+        const { taskIssueNumber } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+            owner,
+            repo,
+            title,
+            labels: ["task"]
+        });
+
+        newTasks.push(`- [ ] #${taskIssueNumber}`)
+    })
+    return newTasks;
+}
+
+const run = async () => {
+    const issueData = parseIssuePayload(payload);
+    const newTasks = mkTaskIssues(issueData.criteria);
+    const body = mkNewBody(issueData, newTasks);
+    const title = `As a ${issueData.persona}, I want to ${issueData.goal}, so that ${issueData.goal}`;
+    const { data } = await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
+        owner,
+        repo,
+        issue_number,
+        title,
+        body,
+        labels: issueData.labels
+    });
+}
+
+run().then(data => {
+    console.log(data);
+})
