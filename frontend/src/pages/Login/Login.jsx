cd31daa9fbbabc76b6570dd8675df3afab8bf74c
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 12:04:09 -0400

    refactor: username into mui textfield

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 // Third Party Components
-import { InputLabel, Input, Button } from "@mui/material";
+import { InputLabel, Input, TextField, Button } from "@mui/material";
+
+// Styling
+import { Stack } from "@mui/material";
 
 const Login = () => {
 	const [username, setUsername] = useState("");
@@ -12,27 +15,28 @@ const Login = () => {
 	}, [username, password]);
 
 	return (
-		<div>
-			<InputLabel htmlFor="username">Username </InputLabel>
-			<Input
-				type="text"
+		<Stack spacing={2}>
+			<TextField
 				id="username"
+				label="Username"
+				variant="standard"
 				placeholder="Enter Username"
 				value={username}
 				onChange={e => setUsername(e.target.value)}
 			/>
-
-			<InputLabel htmlFor="password">Password </InputLabel>
-			<Input
-				type="password"
-				id="password"
-				placeholder="Enter Password"
-				value={password}
-				onChange={e => setPassword(e.target.value)}
-			/>
+			<div>
+				<InputLabel htmlFor="password">Password </InputLabel>
+				<Input
+					type="password"
+					id="password"
+					placeholder="Enter Password"
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+				/>
+			</div>
 			<Button>Username/Password Login</Button>
 			<Button>Common Access Card (CAC) Login</Button>
-		</div>
+		</Stack>
 	);
 };
 
