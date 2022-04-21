1d935291817f1cd18f27bd03d8bedd33e6810b9f
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 11:52:48 -0400

    style: updated App.jsx to be wrapped by MUI container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
-import "./App.css";
-
 // Our Pages and Components
 import Login from "./pages/Login/Login";
 
 // Third Party Components
+import { Container } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div className="App">
-			<h1>Voyager Placeholder</h1>
-			<p>Will show null Outlets until Route elements set up</p>
-			<p>
-				This scaffolding was added in order to just instantiate React Router for
-				the DOM and to give tests something to grab onto as we all work on
-				components and pages.
-			</p>
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/dashboard" />
-			</Routes>
+		<div>
+			<Container>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/dashboard" />
+				</Routes>
+			</Container>
 		</div>
 	);
 }
