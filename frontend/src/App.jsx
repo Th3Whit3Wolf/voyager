f32a31718d097be1811d08fc44ce071876f9953a
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-04-21 13:45:18 -0600

    Feat: Impl AstroUXDS theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,44 @@
 // Our Pages and Components
 import Login from "./pages/Login/Login";
+import Container from "@mui/material/Container";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import getDesignTokens from "./theme.js";
 
 // Third Party Components
-import Container from "@mui/material/Container";
 import { Routes, Route } from "react-router-dom";
-import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import { createContext, useMemo, useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import CssBaseline from "@mui/material/CssBaseline";
 
-// Notes: path="/" will use element Login component from issue43
-// Notes: path='/dashboard' will use element with a changing view based on Role
-//.    this can be most easily achieved through early returns on a Dashboard element
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
-function App() {
+export default function App() {
+	const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+	const [mode, setMode] = useState(prefersDarkMode ? "dark" : "light");
+	const colorMode = useMemo(
+		() => ({
+			// The dark mode switch would invoke this method
+			toggleColorMode: () => {
+				setMode(prevMode => (prevMode === "light" ? "dark" : "light"));
+			}
+		}),
+		[]
+	);
+	const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 	return (
-		<Container>
-			<h1>HEADER PLACEHOLDER</h1>
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/dashboard" />
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
-		</Container>
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Container>
+					<h1>HEADER PLACEHOLDER</h1>
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/dashboard" />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</Container>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
 	);
 }
-
-export default App;
