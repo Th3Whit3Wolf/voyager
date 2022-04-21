3c45f37c26fa32436ed7e9ef7dc8f87861643931
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 15:26:32 -0400

    test: fixed test due to useNavigate in Login for Demo Dashboard

--- frontend/src/pages/Login/Login.test.js
+++ frontend/src/pages/Login/Login.test.js
@@ -4,12 +4,15 @@ import { render, screen } from "@testing-library/react";
 // Import Features to be Tested
 import Login from "./Login.jsx";
 
+// Third Party Wrappers
+import { MemoryRouter as Router } from "react-router-dom";
+
 // Note that I use a setup function here instead of
 // beforeAll or beforeEach due to feedback received
 // from code hardening tools such as SonarCube on
 // previous software projects. --Tony
 
-const setup = () => render(<Login />);
+const setup = () => render(<Router><Login /></Router>);
 
 test("renders a Login component successfully", () => {
 	setup();
