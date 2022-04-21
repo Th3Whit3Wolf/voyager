348e574f98ddcbb86c738da56bbb45438b93bae5
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 13:11:43 -0400

    test: added two memoryrouters to test PageNotFound

--- frontend/src/pages/PageNotFound/PageNotFound.test.js
+++ frontend/src/pages/PageNotFound/PageNotFound.test.js
@@ -1,7 +1,11 @@
 // Import Testing Utilities
 import { render, screen } from "@testing-library/react";
 
+// Third Party Components and Utilities
+import { MemoryRouter } from "react-router-dom";
+
 // Import Features to be Tested
+import App from "../../App.jsx";
 import PageNotFound from "./PageNotFound.jsx";
 
 // Note that I use a setup function here instead of
@@ -10,6 +14,22 @@ import PageNotFound from "./PageNotFound.jsx";
 // previous software projects. --Tony
 
 const setup = () => render(<PageNotFound />);
+
+const rootRouteSetup = () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  )
+}
+
+const brokenRouteSetup = () => {
+  render(
+    <MemoryRouter initialEntries={['/brokenFooBar']}>
+      <App />
+    </MemoryRouter>
+  )
+}
 
 test("renders a PageNotFound component successfully", () => {
 	setup();
