fd9fb52b270b63683e4a74404af1287011677ad0
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 12:22:06 -0400

    feat: added user and pass functionality scaffolding

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,9 +4,27 @@ import React, { useState } from "react";
 import { TextField, Button, Stack } from "@mui/material";
 
 const Login = () => {
+	// STATE
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 
+	// EFFECTS
+
+	// Internal Functions
+
+	const handleUserPass = () => {
+		if (username == undefined || username.length === 0) {
+			setUsername("");
+			alert("Username Required");
+		} else if (password == undefined || password.length === 0) {
+			setPassword("");
+			alert("Password Required");
+		} else {
+			alert(
+				`Entered User: ${username} with Pass: ${password}\n\nDavid would like to handle the Firebase Config file.\n\nUpdate this functionality for auth. after that is complete. Recommend its own Context.`
+			);
+		}
+	};
 	return (
 		<Stack
 			spacing={3}
@@ -26,11 +44,14 @@ const Login = () => {
 				label="Password"
 				variant="standard"
 				placeholder="Enter Password"
+				type="password"
 				value={password}
 				onChange={e => setPassword(e.target.value)}
 				style={{ minWidth: "300px" }}
 			/>
-			<Button variant="contained">Username/Password Login</Button>
+			<Button variant="contained" onClick={handleUserPass}>
+				Username/Password Login
+			</Button>
 			<br />
 			<br />
 			<Button variant="contained">Common Access Card (CAC) Login</Button>
