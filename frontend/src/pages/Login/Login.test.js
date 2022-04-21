ae42175c06698a8a27d121de06c29b9372222497
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 12:24:40 -0400

    feat: issue43 complete pending auth set up. Create new User Story once auth finished.

--- frontend/src/pages/Login/Login.test.js
+++ frontend/src/pages/Login/Login.test.js
@@ -29,16 +29,6 @@ test("issue44: Login displays a username input field that ...", async () => {
 	expect(usernameInput).toBeInTheDocument();
 });
 
-test("issue44: ... accepts text for a username", async () => {
-	setup();
-	const usernameLabel = await screen.findByLabelText(/username/i);
-	const usernameInput = await screen.findByPlaceholderText(/enter username/i);
-	expect(usernameLabel).toBeInTheDocument();
-	expect(usernameInput).toBeInTheDocument();
-	userEvent.type(usernameInput, "scruffy");
-	expect(usernameInput.value).toBe("scruffy");
-});
-
 test("issue45: Login displays a password field that accepts a password", async () => {
 	setup();
 	const passwordLabel = await screen.findByLabelText(/password/i);
