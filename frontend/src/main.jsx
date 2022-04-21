e629874e347066e51aa2dc31246d8bcd876a5fec
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 09:02:27 -0400

    feat: implemented SPA Router around application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Third Party Components
+import { BrowserRouter as Router } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<App />
+		</Router>
 	</React.StrictMode>
 );
