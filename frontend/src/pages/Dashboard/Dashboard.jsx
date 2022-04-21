7d0b45ad1612d4f376682a3febe265679d15e78b
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 14:15:17 -0400

    refactor: adjusted some syntax

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,2 +1,14 @@
 // No tests yet, since there is no real dashboard.
 // This is a demo dashboard as a placeholder
+
+import React from "react";
+
+const Dashboard = () => {
+	return (
+		<>
+			<h3>DASHBOARD</h3>
+		</>
+	);
+};
+
+export default Dashboard;
