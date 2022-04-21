30e59a7a98028e94a60c2139717b4b73a98df884
tkbdsi <patient-remix-0q@icloud.com>
2022-04-21 14:56:40 -0400

    chore: stash to install mui lab

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
 // No tests yet, since there is no real dashboard.
 // This is a demo dashboard as a placeholder
+// when actual dashboards are built up, switch to TDD
 
 import React from "react";
+
+// Third Party Components and Utilities
+import { Tab, TabContext, TabList, TabPanel, Stack } from "@mui/material";
 
 // NOTE: Currently Dashboard is taking a prop called
 // role. This is coming from Login.jsx where the Fake
@@ -19,10 +23,43 @@ import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
 	const location = useLocation(); // props are being passed with navigate, so I need useLocation go grab them
+	const role = location.state.role;
 
- 	return (
+	// Setting up Different Views Based on Role
+	// There are many ways to do conditional views, but with
+	// so many possible concurrent views, early returns might be the best
+	// way with a final return that always shows in the event all other
+	// conditionals do not trigger
+
+	// User View ... this conditional compares to role from location, should be changed to AUTH obj later
+	if (role === "user") {
+		return (
+			<>
+				<h1>User's Dashboard</h1>
+					<div>
+						<h3>Outprocessing Tasks</h3>
+					</div>
+					<div>
+						<h3>Inprocessing Tasks</h3>
+					</div>
+			</>
+		);
+	}
+
+	// Generic Admin View ... this conditional compares to role from location, should be changed to AUTH obj later
+	if (role === "admin") {
+		return <h1>Admin's Dashboard</h1>;
+	}
+
+	// Installation Admin View
+
+	// Site Admin View
+
+	// Generic View to Show if All Else Fails
+	return (
 		<>
-			<h3>{location.state.role.toUpperCase()} DEMO DASHBOARD</h3>
+			<h2>This is a Default Catchall View</h2>
+			<p>Somehow authenticated but not as a viable role and made it here.</p>
 		</>
 	);
 };
