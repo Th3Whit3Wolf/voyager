23f7c419515e3a1d3ccca17515f7bbf033ea1205
tkbdsi <patient-remix-0q@icloud.com>
2022-04-22 12:55:05 -0400

    test: imported mock data into mock fetch for use

--- frontend/src/hooks/useFetchMock.js
+++ frontend/src/hooks/useFetchMock.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { mockUserData } from "../data/mockUserData";
 
 const useFetchMock = (mockUrlRoute, revision) => {
+	console.log(mockUserData);
+
 	const [data, setData] = useState([]);
 	const [error, setError] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
