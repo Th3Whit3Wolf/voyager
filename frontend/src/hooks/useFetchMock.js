788309d6f65427004790eb7bd8b5ce0c7a1f5d82
tkbdsi <patient-remix-0q@icloud.com>
2022-04-22 12:41:14 -0400

    feat: :sparkles: added stateful return. added props

--- frontend/src/hooks/useFetchMock.js
+++ frontend/src/hooks/useFetchMock.js
@@ -1,1 +1,11 @@
 import React, { useState, useEffect } from "react";
+
+const useFetchMock = (mockUrlRoute, revision) => {
+	const [data, setData] = useState([]);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	return { data, error, isLoading };
+};
+
+export default useFetchMock;
