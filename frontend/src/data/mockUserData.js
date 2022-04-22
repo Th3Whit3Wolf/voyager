95927c8c5c739d6d7efe6fed062982c366665d68
tkbdsi <patient-remix-0q@icloud.com>
2022-04-22 12:50:55 -0400

    test: started creating a possible JSON object format (See details)
    not sure where the POC name, phone, and email are stored in the ERD. I am making this mock data on my best interpretation of the ERD, however I am not an expert in Space Force structures.

---
+++ frontend/src/data/mockUserData.js
@@ -0,0 +1,12 @@
+export const mockUserData = [
+	{
+		id: 1,
+		title: "Form 55",
+		description: "See Squadron Safety Rep to update Form 55",
+		task_type: "Outprocessing",
+		approver: "Capt Safety Pants",
+		created_at: new Date("July 1 2002 11:53"),
+		updated_at: new Date("July 1 2002 11:53"),
+		owner: "Delta"
+	}
+];
