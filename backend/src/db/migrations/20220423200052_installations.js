43939b16e75b42c40484c719ad38961cf6454a9e
PeeWee <AlphaSherman@gmail.com>
2022-04-23 17:12:06 -0400

    feat: build installatins table

---
+++ backend/src/db/migrations/20220423200052_installations.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.createTable("installations", function (table) {
+		table.increments("id");
+		table.text("name");
+		table.text("location");
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists("installations");
+};
