c5763fa6dba34077d1bccc7246e59639423cf0a5
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-04-24 00:32:01 -0600

    Feat: Added installations seed data

--- backend/src/db/seeds/index.js
+++ backend/src/db/seeds/index.js
@@ -1,4 +1,8 @@
-const { PrismaClient } = require("@prisma/client");
+import Prisma from "@prisma/client";
+
+import { createRequire } from "module";
+
+const { PrismaClient } = Prisma;
 
 const PG_USER = process.env.PG_USER || "postgres";
 const PG_PASSWORD = process.env.PG_PASSWORD || "docker";
@@ -11,6 +15,9 @@ const URI =
 		? `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${DB_NAME}?schema=public`
 		: process.env.DATABASE_URL;
 
+const require = createRequire(import.meta.url);
+const installations = require("./installations.json");
+
 const prisma = new PrismaClient({
 	datasources: {
 		db: {
@@ -20,79 +27,16 @@ const prisma = new PrismaClient({
 });
 
 const createInstallations = async () => {
-	const buckley = await prisma.Installation.upsert({
-		where: { name: "Buckley Space Force Base" },
-		update: {},
-		create: {
-			name: "Buckley Space Force Base",
-			location: "Aurora, Colorado"
-		}
-	});
+	installations.forEach(async installation => {
+		const entry = await prisma.Installation.upsert({
+			where: { location: installation.location },
+			update: {},
+			create: {
+				...installation
+			}
+		});
 
-	/*
-	const losAngelos = await prisma.Installation.upsert({
-		name: "Los Angeles Air Force Base",
-		location: "El Segundo, California"
-	});
-
-	const patrick = await prisma.Installation.upsert({
-		name: "Patrick Space Force Base",
-		location: "Brevard County, Florida"
-	});
-
-	const peterson = await prisma.Installation.upsert({
-		name: "Peterson Space Force Base",
-		location: "Colorado Springs, Colorado"
-	});
-
-	const schriever = await prisma.Installation.upsert({
-		name: "Schriever Space Force Base",
-		location: "Colorado Springs, Colorado"
-	});
-
-	const vandenberg = await prisma.Installation.upsert({
-		name: "Vandenberg Space Force Base",
-		location: "Santa Barbara County, California"
-	});
-
-	const capeCanaveral = await prisma.Installation.upsert({
-		name: "Cape Canaveral Space Force Station",
-		location: "Cape Canaveral, Florida"
-	});
-
-	const capeCod = await prisma.Installation.upsert({
-		name: "Cape Cod Space Force Station",
-		location: "Bourne, Massachusetts"
-	});
-
-	const cavalier = await prisma.Installation.upsert({
-		name: "Cavalier Space Force Station",
-		location: "Cavalier, North Dakota"
-	});
-
-	const cheyenneMountain = await prisma.Installation.upsert({
-		name: "Cheyenne Mountain Space Force Station",
-		location: "Colorado Springs, Colorado"
-	});
-
-	const clear = await prisma.Installation.upsert({
-		name: "Clear Space Force Station",
-		location: "Clear, Alaska"
-	});
-
-	const kaenaPoint = await prisma.Installation.upsert({
-		name: "Kaena Point Space Force Station",
-		location: "Honolulu County, Hawaii"
-	});
-
-	const newBoston = await prisma.Installation.upsert({
-		name: "New Boston Space Force Station",
-		location: "New Boston, New Hampshire"
-	});
-    */
-
-	console.log({
-		buckley
+		console.log(entry);
 	});
 };
 
