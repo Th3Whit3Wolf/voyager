16cf070830c1661f04d9df67daffbd97a57fa071
tkbdsi <patient-remix-0q@icloud.com>
2022-04-24 07:54:17 -0400

    chore: initial commit of Splash Screen scaffolding

---
+++ frontend/src/components/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SplashScreen = () => {
+	return (
+		<div>
+			<h3>Spash Screen Scaffolding</h3>
+			<p>In the Design Phase -- Kelly</p>
+		</div>
+	);
+};
+
+export default SplashScreen;
