3f7e81813465841006bf3df5740c747556939003
tkbdsi <patient-remix-0q@icloud.com>
2022-04-24 19:41:13 -0400

    style: determined how to blackout screen to override MUI

--- frontend/src/components/SplashScreen/SplashScreen.jsx
+++ frontend/src/components/SplashScreen/SplashScreen.jsx
@@ -4,9 +4,11 @@ import styles from "./SplashScreen.module.css";
 
 const SplashScreen = () => {
 	return (
-		<div className={styles.center}>
-			<h3>Spash Screen Scaffolding</h3>
-			<p>In the Design Phase -- Kelly</p>
+		<div className={styles.blackout}>
+			<div className={styles.center}>
+				<h3>Spash Screen Scaffolding</h3>
+				<p>In the Design Phase -- Kelly</p>
+			</div>
 		</div>
 	);
 };
