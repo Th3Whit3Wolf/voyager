361e91cf05310721fcb559afcce18eab766b0669
tkbdsi <patient-remix-0q@icloud.com>
2022-04-26 16:20:50 -0600

    style: fixed prettier lint

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,15 +5,16 @@ const app = express();
 
 app.use(express.json());
 
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
 	res.header({
-		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+		"Access-Control-Allow-Headers":
+			"Origin, X-Requested-With, Content-Type, Accept"
 	});
-    res.header({
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    });
+	res.header({
+		"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	});
 	next();
-})
+});
 
 app.get("/status", (req, res) => {
 	res.send("good");
