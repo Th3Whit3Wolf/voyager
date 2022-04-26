ff43893817f7df62a0580a1065410414e8e754a3
tkbdsi <patient-remix-0q@icloud.com>
2022-04-26 16:11:37 -0600

    refactor: added content type header to set up specific media returns later

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use( (req, res, next) => {
 	res.header({
-		'Access-Control-Allow-Headers': 'Origin, X-Requested-With'
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type'
 	})
 	next();
 })
