8cd6279145ae5a625df02d6ca147561fbed28273
tkbdsi <patient-remix-0q@icloud.com>
2022-04-26 16:15:32 -0600

    feat: added CORS safe methods, see details for spec link
    see https://fetch.spec.whatwg.org/#http-access-control-allow-methods

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,10 @@ app.use(express.json());
 app.use( (req, res, next) => {
 	res.header({
 		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-	})
+	});
+    res.header({
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    });
 	next();
 })
 
