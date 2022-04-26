0104e9ee47ff48aadc9f6f3be68833d0d90501f0
tkbdsi <patient-remix-0q@icloud.com>
2022-04-26 16:12:46 -0600

    refactor: added final header to specify what is allowed for acceptance

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use( (req, res, next) => {
 	res.header({
-		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type'
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
 	})
 	next();
 })
