73b7f1b8ed7cec6ffda336e59c04c0a7dcca8640
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-04-27 12:33:13 -0600

    Feat: Added seed status logger

--- backend/src/db/seeds/utils.js
+++ backend/src/db/seeds/utils.js
@@ -1,3 +1,6 @@
+import { Console } from "console";
+import { Transform } from "stream";
+
 const PG_USER = process.env.PG_USER || "postgres";
 const PG_PASSWORD = process.env.PG_PASSWORD || "docker";
 const PG_PORT = process.env.PG_PORT || 5432;
@@ -15,4 +18,36 @@ const log = {
 	}
 };
 
-export { log, URI };
+const statusUpdate = (schemaName, data) => {
+	// @see https://stackoverflow.com/a/67859384
+	const ts = new Transform({
+		transform(chunk, enc, cb) {
+			cb(null, chunk);
+		}
+	});
+	const logger = new Console({ stdout: ts });
+	logger.table(data);
+	const table = (ts.read() || "").toString();
+	let result = "";
+	table.split(/[\r\n]+/).forEach(row => {
+		let r = row.replace(/[^┬]*┬/, "├");
+		r = r.replace(/┐/g, "┤");
+		r = r.replace(/^├─*┼/, "├");
+		r = r.replace(/│[^│]*/, "");
+		r = r.replace(/^└─*┴/, "└");
+		r = r.replace(/'/g, " ");
+		result += `${r}\n`;
+	});
+
+	const title = `Table: ${schemaName}`;
+	const resultLineLength = result.split("\n")[0].length;
+	const leftIndent = Math.ceil((resultLineLength - title.length) / 2 - 1);
+	const rightIndent = Math.floor((resultLineLength - title.length) / 2 - 1);
+	console.log(
+		`┌${"─".repeat(resultLineLength - 2)}┐\n│${" ".repeat(
+			leftIndent
+		)}${title}${" ".repeat(rightIndent)}│\n${result}`
+	);
+};
+
+export { log, statusUpdate, URI };
