1c89d7b26bdc56cfdd34b44a97065785b7d54c3a
tkbdsi <patient-remix-0q@icloud.com>
2022-04-27 13:53:22 -0400

    style: added charcter widths to inputs
    Co-authored-by: MoonKeeper0 <MoonKeeper0@users.noreply.github.com>

--- frontend/src/components/TableRow/AdminTableRow/AdminTableRow.jsx
+++ frontend/src/components/TableRow/AdminTableRow/AdminTableRow.jsx
@@ -5,7 +5,8 @@ import {
 	TableRow,
 	TableCell,
 	Switch,
-	Button
+	Button,
+	TextField
 } from "@mui/material";
 
 const AdminTableRow = ({ entry }) => {
@@ -16,17 +17,30 @@ const AdminTableRow = ({ entry }) => {
 					onChange={() => console.log(`Switch has been changed id ${entry.id}`)}
 				/>
 			</TableCell>
-			<TableCell>{entry.title}</TableCell>
-			<TableCell>{entry.description}</TableCell>
-			<TableCell>{entry.approver}</TableCell>
-			<TableCell>Not in Current ERD</TableCell>
-			<TableCell>Not in Current ERD</TableCell>
+			<TableCell>
+				<TextField placeholder={entry.title} sx={{ width: "20ch" }} />
+			</TableCell>
+			<TableCell>
+				<TextField value={entry.description} sx={{ width: "45ch" }} />
+			</TableCell>
+			<TableCell>
+				<TextField placeholder={entry.approver} sx={{ width: "25ch" }} />
+			</TableCell>
+			<TableCell>
+				<TextField placeholder={"Not in Current ERD"} sx={{ width: "25ch" }} />
+			</TableCell>
+			<TableCell>
+				<TextField placeholder={`Not in Current ERD`} sx={{ width: "25ch" }} />
+			</TableCell>
 			<TableCell>
 				{`${
 					entry.updated_at.getUTCMonth() + 1
 				} - ${entry.updated_at.getUTCDate()} - ${entry.updated_at.getUTCFullYear()}`}
 			</TableCell>
-			<TableCell>{entry.owner}</TableCell>
+			<TableCell>
+				<TextField placeholder={entry.owner} sx={{ width: "10ch" }} />
+				{/* should be a drop down once able to pull all the base names */}
+			</TableCell>
 			<TableCell>
 				<Button onClick={() => console.log(`Item Deleted id ${entry.id}`)}>
 					Delete
