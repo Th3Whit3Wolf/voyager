876bf2aee2dde9772224dcf7e2bedaeb54f1b592
tkbdsi <patient-remix-0q@icloud.com>
2022-04-27 12:18:58 -0400

    refactor: added data return for admin route on mock fetch

--- frontend/src/hooks/useFetchMock.js
+++ frontend/src/hooks/useFetchMock.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { mockUserData } from "../data/mockUserData";
+import { mockAdminData } from "../data/mockUserData";
 
 const useFetchMock = (mockUrlRoute, revision, delay) => {
 	const [data, setData] = useState([]);
@@ -14,9 +15,13 @@ const useFetchMock = (mockUrlRoute, revision, delay) => {
 			}, delay);
 		});
 
-	if (mockUrlRoute === "/api/mock/admin") {
-		console.log("No mockAdinData in data folder yet.");
-	}
+	if (mockUrlRoute === "/api/mock/admin")
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				setData(mockAdminData);
+				setIsLoading(false);
+			}, delay);
+		});
 
 	return { data, error, isLoading };
 };
