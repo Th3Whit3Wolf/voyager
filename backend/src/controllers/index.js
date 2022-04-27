5ad126835092fbf8b9d254edfd0afc7ae827c0fb
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-04-27 06:59:08 -0600

    Fix: imports

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,6 +1,3 @@
-import Commands from "./command";
-import Installations from "./installation";
-import Deltas from "./delta";
 import Squadrons from "./squadrons";
 
-export { Commands, Deltas, Installations, Squadrons };
+export { Squadrons };
