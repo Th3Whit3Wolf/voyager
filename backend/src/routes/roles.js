03ecb335c1890363c42a9dfdefa0e65c7e04d43f
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-04-27 12:37:11 -0600

    Feat: Added roles endpoint

---
+++ backend/src/routes/roles.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { Roles } from "../controllers";
+
+const router = Router();
+
+router.get("/roles", Roles.getAll);
+router.get("/roles/:roleID", Roles.getByID);
+router.get("/roles/users", Roles.getAllUsers);
+router.get("/roles/admins", Roles.getAllAdmins);
+router.get("/roles/admins/site", Roles.getAllSiteAdmins);
+router.get("/roles/admins/installation", Roles.getAllSiteAdmins);
+router.get("/roles/admins/command", Roles.getAllCommandAdmins);
+router.get("/roles/admins/delta", Roles.getAllDeltaAdmins);
+router.get("/roles/admins/squadron", Roles.getAllSquadronAdmins);
+
+export default router;
