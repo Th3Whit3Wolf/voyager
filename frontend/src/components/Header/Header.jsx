a6c2afecbd1b9bd1d95486866b37d60988edb238
tkbdsi <patient-remix-0q@icloud.com>
2022-04-27 12:06:13 -0400

    fix: added logo to Header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Header.module.css";
+import logo from "../../space-force-logo.png";
 
 const Header = () => {
 	return (
 		<header className={styles.header__container}>
-			<img src="/Images/space-force-logo.png" alt="Space Force Logo" />
+			<img src={logo} alt="Space Force Logo" />
 			<h1>
 				<span className={styles.header__title2}>Voyager</span>
 			</h1>
