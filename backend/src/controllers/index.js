1cd40ec8f87ab9ce48ded546d84e9057a618f757
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-04-28 14:04:46 -0600

    Feat: Added Controller for Users

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -3,5 +3,6 @@ import Deltas from "./delta";
 import Installations from "./installation";
 import Roles from "./roles";
 import Squadrons from "./squadrons";
+import Users from "./users";
 
-export { Commands, Deltas, Installations, Roles, Squadrons };
+export { Commands, Deltas, Installations, Roles, Squadrons, Users };
