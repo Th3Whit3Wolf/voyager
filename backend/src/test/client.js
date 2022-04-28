ca3bc2ebdc3080caab5111a1a5b37ce4b4c53020
David Karrick <48275422+Th3Whit3Wolf@users.noreply.github.com>
2022-04-28 11:52:02 -0600

    Delete client.js

--- backend/src/test/client.js
+++
@@ -1,7 +0,0 @@
-// teamplting for future Prisma Method Test (from https://www.prisma.io/docs/guides/testing/unit-testing)
-// not fully implemente since custom Prisma functions not complete yet
-
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-export default prisma;
