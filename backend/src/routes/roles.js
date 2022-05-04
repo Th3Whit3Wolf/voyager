f2942ac0128b57b023959127d5016b16808e76d6
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-03 20:45:27 -0600

    Chore: Simplify Role routes

--- backend/src/routes/roles.js
+++ backend/src/routes/roles.js
@@ -4,13 +4,6 @@ import { Roles } from "../controllers";
 const router = Router();
 
 router.get("/roles", Roles.getAll);
-router.get("/roles/users", Roles.getAllUsers);
-router.get("/roles/admins", Roles.getAllAdmins);
-router.get("/roles/admins/site", Roles.getAllSiteAdmins);
-router.get("/roles/admins/installation", Roles.getAllSiteAdmins);
-router.get("/roles/admins/command", Roles.getAllCommandAdmins);
-router.get("/roles/admins/delta", Roles.getAllDeltaAdmins);
-router.get("/roles/admins/squadron", Roles.getAllSquadronAdmins);
 router.get("/roles/:roleID", Roles.getByID);
 
 export default router;
