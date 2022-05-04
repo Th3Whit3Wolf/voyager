6ce1a39d2c9b0b878c89431a7a48bb6787c6042a
tkbdsi <patient-remix-0q@icloud.com>
2022-05-04 09:16:07 -0400

    test: added testing route for future hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,6 +40,7 @@ export default function App() {
 					<Routes>
 						<Route path="/" element={<Login />} />
 						<Route path="/dashboard" element={<Dashboard />} />
+						<Route path="/test/fetch" element={null} />
 						<Route path="*" element={<PageNotFound />} />
 					</Routes>
 				</Container>
