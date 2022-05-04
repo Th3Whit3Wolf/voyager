9a43736b149923c7ea46f68dda724abb08b24ee8
tkbdsi <patient-remix-0q@icloud.com>
2022-05-04 16:07:15 -0400

    refactor: created index list of components for export

---
+++ frontend/src/components/index.js
@@ -0,0 +1,27 @@
+import DeleteDialog from "./Dialog/DeleteDialog/DeleteDialog";
+import InfoDialog from "./Dialog/InfoDialog/InfoDialog";
+
+import Header from "./Header/Header";
+import Loading from "./Loading/Loading";
+import SplashScreen from "./SplashScreen/SplashScreen";
+
+import AdminTableRow from "./TableRow/AdminTableRow/AdminTableRow";
+import ModifyAdminRow from "./TableRow/ModifyAdminRow/ModifyAdminRow";
+import UserTableRow from "./TableRow/UserTableRow/UserTableRow";
+
+import AdminTable from "./Tables/AdminTable/AdminTable";
+import UserTable from "./Tables/UserTable/UserTable";
+
+import UserSettings from "./UserSettings/UserSettings";
+
+export {
+	Header,
+	Loading,
+	SplashScreen,
+	AdminTableRow,
+	ModifyAdminRow,
+	UserTableRow,
+	AdminTable,
+	UserTable,
+	UserSettings
+};
