a95abbf279f07b18969652b2dd07d1029bd5e39e
tkbdsi <patient-remix-0q@icloud.com>
2022-05-04 16:39:13 -0400

    test: added failing test for last name

--- frontend/src/components/UserSettings/UserSettings.jsx
+++ frontend/src/components/UserSettings/UserSettings.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const UserSettings = () => {
-	return <h1>User Settings</h1>;
+const UserSettings = ({ settings }) => {
+	return (
+		<div>
+			<h1>User Settings</h1>
+			<ul>
+				<li> First Name: {settings.firstName}</li>
+			</ul>
+		</div>
+	);
 };
 
 export default UserSettings;
--- frontend/src/components/UserSettings/UserSettings.test.js
+++ frontend/src/components/UserSettings/UserSettings.test.js
@@ -33,34 +33,13 @@ test("renders a first name successfully", async () => {
 	expect(firstName).toBeInTheDocument();
 });
 
-// test("issue44: Login displays a username input field that ...", async () => {
-// 	setup();
-// 	const usernameLabel = await screen.findByLabelText(/username/i);
-// 	const usernameInput = await screen.findByPlaceholderText(/enter username/i);
-// 	expect(usernameLabel).toBeInTheDocument();
-// 	expect(usernameInput).toBeInTheDocument();
-// });
-
-// test("issue45: Login displays a password field that accepts a password", async () => {
-// 	setup();
-// 	const passwordLabel = await screen.findByLabelText(/password/i);
-// 	const passwordInput = await screen.findByPlaceholderText(/enter password/i);
-// 	expect(passwordLabel).toBeInTheDocument();
-// 	expect(passwordInput).toBeInTheDocument();
-// });
-
-// test("issue46: Login displays a Login Button that takes username and password for authentication", async () => {
-// 	setup();
-// 	const loginButton = await screen.getByRole("button", {
-// 		name: /username\/password login/i
-// 	});
-// 	expect(loginButton).toBeInTheDocument();
-// });
-
-// test("issue47: Login displays a CAC Login button that takes CAC for authentication (note: this is faked for MVP)", async () => {
-// 	setup();
-// 	const cacButton = await screen.findByRole("button", {
-// 		name: /common access card \(cac\) login/i
-// 	});
-// 	expect(cacButton).toBeInTheDocument();
-// });
+test("renders a last name successfully", async () => {
+	const stgs = {
+		id: "68",
+		firstName: "Bridget",
+		lastName: "Smitham"
+	};
+	setup(stgs);
+	const lastName = await screen.findByText(/smitham/i);
+	expect(lastName).toBeInTheDocument();
+});
