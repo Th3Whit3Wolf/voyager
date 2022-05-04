a22ef1b9d348a233e9b3242533745789e6707a65
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-04 12:04:59 -0600

    Tests: Better ID test

--- backend/src/server.test.js
+++ backend/src/server.test.js
@@ -1,11 +1,8 @@
 import request from "supertest";
 import app from "./server";
-// import routes from "./routes";
 
 const response = request(app);
-
 const getRoutes = ["roles", "tasks", "units", "users", "users/tasks"];
-/*
 const firstID = {
 	roles: {
 		kind: "SITE_ADMIN"
@@ -40,7 +37,7 @@ const firstID = {
 		progress: "NOT_STARTED"
 	}
 };
-*/
+
 describe("GET Tests", () => {
 	test("/status should be good", async () => {
 		const res = await response.get("/status");
@@ -56,58 +53,13 @@ describe("GET Tests", () => {
 
 		test(`GET /api/v1/${route}/1`, async () => {
 			const res = await response.get(`/api/v1/${route}/1`);
-			expect(res.statusCode).toBe(200);
-			// const { json } = res;
-			// expect(res.headers["Content-Type"]).toMatch(/json/);
-			console.log("Response Text: ", res.text);
-			console.log("Response Body: ", res.body);
+			const { body } = res;
 
-			// expect(json.data.id).toBe(1);
-			// Object.entries(firstID[route]).forEach(([k, v]) => {
-			//	expect(json.data[k]).toBe(v);
-			// });
+			expect(res.statusCode).toBe(200);
+			expect(body.data.id).toBe(1);
+			Object.entries(firstID[route]).forEach(([k, v]) => {
+				expect(body.data[k]).toBe(v);
+			});
 		});
 	});
-
-	/*
-	const idPairs = {
-		roles: { id: 1, check: { label: "kind", value: "SITE_ADMIN" } },
-		tasks: { id: 1, check: { label: "title", value: "Finance" } },
-		units: {
-			id: 1,
-			check: { label: "name", value: "Space Operations Command" }
-		},
-		users: {
-			id: 1,
-			check: { label: "email", value: "rick.sanchez@spaceforce.mil" }
-		}
-	};
-
-	/// Read all exported routes and use them
-
-	Object.keys(routes).forEach(route => {
-		test(`test full path return of getAll: ${route}`, async () => {
-			const res = await response.get(`/api/v1/${route}`);
-			// console.log(`/api/v1/${route} yields ->${res.text}`);
-			expect(res.statusCode).toBe(200);
-		});
-		test(`test id specific path return of : ${route}`, async () => {
-			const res = await response.get(
-				`/api/v1/${route}/${idPairs[route].id}`
-			);
-			// console.log(
-			// 	`/api/v1/${route}/${idPairs[route].id} yields ->${res.text}`
-			// );
-			// console.log(`--${route}--${route === "roles"}`);
-			if (route === "roles") {
-				console.log(res.text);
-			}
-			expect(res.statusCode).toBe(200);
-			// const paramName = idPairs[route].check.label;
-			// expect(JSON.parse(res.text)[0][paramName]).toBe(
-			//	idPairs[route].check.value
-			// );
-		});
-	});
-	*/
 });
