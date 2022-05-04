31b70b0b6b2ace50e25ed42ed082cd86288926ee
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-04 11:57:52 -0600

    Tests: Update testing

--- backend/src/server.test.js
+++ backend/src/server.test.js
@@ -4,6 +4,41 @@ import routes from "./routes";
 
 const response = request(app);
 
+const getRoutes = ["roles", "tasks", "units", "users", "users/tasks"];
+const firstID = {
+	roles: {
+		kind: "SITE_ADMIN"
+	},
+	tasks: {
+		title: "Finance",
+		description:
+			"Schedule appointment for Finance in-process brief to complete PCS/PCS Travel voucher.",
+		isActive: true,
+		kind: "IN_PROCESSING"
+	},
+	units: {
+		name: "Space Operations Command",
+		abbrev: "SpOC",
+		kind: "COMMAND",
+		function:
+			"Space, cyber, and intelligence operations, and combat support"
+	},
+	users: {
+		firstName: "Rick",
+		lastName: "Sanchez",
+		email: "rick.sanchez@spaceforce.mil",
+		dsn: "(312) 867-5309",
+		auth: "",
+		status: "STATIONARY",
+		assignedUnitID: 1,
+		assignedOfficeSymbol: "MSOS"
+	},
+	"users/tasks": {
+		taskID: 1,
+		userID: 68,
+		progress: "NOT_STARTED"
+	}
+};
 describe("GET Tests", () => {
 	test("/status should be good", async () => {
 		const res = await response.get("/status");
@@ -11,6 +46,28 @@ describe("GET Tests", () => {
 		expect(res.statusCode).toBe(200);
 	});
 
+	getRoutes.forEach(route => {
+		test(`GET /api/v1/${route}`, async () => {
+			const res = await response.get(`/api/v1/${route}`);
+			expect(res.statusCode).toBe(200);
+		});
+
+		test(`GET /api/v1/${route}/1`, async () => {
+			const res = await response.get(`/api/v1/${route}/1`);
+			expect(res.statusCode).toBe(200);
+			// const { json } = res;
+			// expect(res.headers["Content-Type"]).toMatch(/json/);
+			console.log("Response Text: ", res.text);
+			console.log("Response Body: ", res.body);
+
+			// expect(json.data.id).toBe(1);
+			// Object.entries(firstID[route]).forEach(([k, v]) => {
+			//	expect(json.data[k]).toBe(v);
+			// });
+		});
+	});
+
+	/*
 	const idPairs = {
 		roles: { id: 1, check: { label: "kind", value: "SITE_ADMIN" } },
 		tasks: { id: 1, check: { label: "title", value: "Finance" } },
@@ -25,6 +82,7 @@ describe("GET Tests", () => {
 	};
 
 	/// Read all exported routes and use them
+
 	Object.keys(routes).forEach(route => {
 		test(`test full path return of getAll: ${route}`, async () => {
 			const res = await response.get(`/api/v1/${route}`);
@@ -49,4 +107,5 @@ describe("GET Tests", () => {
 			// );
 		});
 	});
+	*/
 });
