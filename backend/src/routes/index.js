738cf02a0dd5fa8beb15afb7036b3d851d484ddb
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-03 20:46:18 -0600

    Feat: Added routes for Task

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,15 +1,9 @@
-import commands from "./commands";
-import deltas from "./deltas";
-import installations from "./installations";
 import roles from "./roles";
-import squadrons from "./squadrons";
+import units from "./units";
 import users from "./users";
 
 export default {
-	commands,
-	deltas,
-	installations,
 	roles,
-	squadrons,
+	units,
 	users
 };
