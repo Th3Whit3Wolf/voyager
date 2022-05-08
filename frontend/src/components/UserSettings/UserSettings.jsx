a4a8260871dfad32db8fb17ce0034adc4ea36cd2
tkbdsi <patient-remix-0q@icloud.com>
2022-05-08 19:58:48 -0400

    feat: user settings now contains basic contact info

--- frontend/src/components/UserSettings/UserSettings.jsx
+++ frontend/src/components/UserSettings/UserSettings.jsx
@@ -2,27 +2,30 @@
 import React from "react";
 
 // Third Part Components
-import { Card, CardHeader } from "@mui/material";
+import { Card, CardHeader, CardContent, Typography } from "@mui/material";
 
 const UserSettings = ({ settings }) => {
 	console.log(settings);
 
 	return (
 		<div>
-			<h1>User Settings</h1>
-			<ul>
-				<li>
-					{" "}
-					{settings.firstName} {settings.lastName}
-				</li>
-				<li> Email: {settings.email}</li>
-				<li> DSN: {settings.dsn}</li>
-			</ul>
+			<h2>{settings.firstName + " " + settings.lastName}</h2>
 			<Card sx={{ maxWidth: 350 }}>
-				<CardHeader
-					title={settings.firstName + " " + settings.lastName}
-					subheader={settings.assignedOfficeSymbol}
-				/>
+				<CardHeader title={"Current Unit: " + settings.assignedOfficeSymbol} />
+				<CardContent>
+					<Typography>Your Contact Information</Typography>
+					<Typography>DSN: {settings.dsn}</Typography>
+					<Typography>Email: {settings.email}</Typography>
+				</CardContent>
+				<CardContent>
+					<Typography>Supervisor Contact Information</Typography>
+					<Typography>
+						Name:{" "}
+						{settings.supervisor.firstName + " " + settings.supervisor.lastName}
+					</Typography>
+					<Typography>DSN: {settings.supervisor.dsn}</Typography>
+					<Typography>Email: {settings.supervisor.email}</Typography>
+				</CardContent>
 			</Card>
 		</div>
 	);
