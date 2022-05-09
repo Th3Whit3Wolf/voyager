48bf2dc0f51c4c75054fd971cce01752af90bdd1
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-09 14:02:47 -0600

    Feat: Better error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,67 @@
+import { createTerminus } from "@godaddy/terminus";
+import http from "http";
 import app from "./server";
+import logger from "./logger";
+import { db } from "./controllers/Controller";
 
 const port = process.env.PORT || 8081;
+const NODE_ENV = process.env.NODE_ENV || "Development";
 
-const start = p => {
-	try {
-		app.listen(p, () => {
-			console.log(`Api running at http://localhost:${p}  🚀`);
-		});
-	} catch (err) {
-		console.error(err);
-		process.exit();
-	}
+const server = http.createServer(app);
+
+const onListening = () => {
+	logger.info({
+		status: NODE_ENV,
+		msg: `Server started  🚀`,
+		url: `http://localhost:${port}`
+	});
 };
 
-start(port);
+const onSignal = async () => {
+	logger.info("Server is starting cleanup");
+
+	db.$disconnect()
+		.then(() => logger.info("database disconnected"))
+		.catch(err => logger.error({ err, msg: "error during disconnection" }));
+};
+
+const onShutdown = async () => {
+	logger.info("cleanup finished, server is shutting down");
+};
+
+const onError = err => {
+	logger.error(err);
+	process.exit(1);
+};
+
+const main = async () => {
+	process.on("unhandledRejection", (err, promise) => {
+		logger.error({ err, msg: `Unhandled Rejection at: ${promise}` });
+		// send error to your error tracking software here
+		process.exit(1);
+	});
+
+	process.on("uncaughtException", (err, origin) => {
+		logger.error({
+			err,
+			msg: `Uncaught Exception: ${err.message} at: ${origin}`
+		});
+		// send error to your error tracking software here
+		process.exit(1);
+	});
+
+	await db.$connect();
+
+	createTerminus(server, {
+		onSignal,
+		onShutdown,
+		logger: (msg, err) => logger.error({ msg, err }),
+		healthChecks: {
+			__unsafeExposeStackTraces: NODE_ENV !== "production"
+		}
+	});
+
+	server.listen(port, onListening);
+};
+
+main().catch(onError);
