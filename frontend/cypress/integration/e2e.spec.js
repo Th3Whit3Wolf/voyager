68014ce6cb92c73cb87c18f77fb663bc71c3bc13
tkbdsi <patient-remix-0q@icloud.com>
2022-05-10 12:01:32 -0400

    refactor: place all 2e2 tests into one access file

--- frontend/cypress/integration/e2e.spec.js
+++ frontend/cypress/integration/e2e.spec.js
@@ -62,3 +62,16 @@ describe("testing the Login View", () => {
 		cy.url().should("eq", "http://localhost:3000/dashboard");
 	});
 });
+
+describe("testing the Login View", () => {
+	beforeEach(() => {
+		cy.visit("http://localhost:3000/");
+	});
+
+	it("entering correct email and password logs a User to a Dashboard", () => {
+		cy.get("input").first().type("sherri.ortiz@spaceforce.mil");
+		cy.get("input").last().type("1234567890qwertyuiop");
+		cy.get("button").first().click();
+		cy.url().should("eq", "http://localhost:3000/dashboard");
+	});
+});
