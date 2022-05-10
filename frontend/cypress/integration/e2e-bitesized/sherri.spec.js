ba7ed47e3ff27c6982dc82add32b2be46e2b4219
tkbdsi <patient-remix-0q@icloud.com>
2022-05-10 12:21:21 -0400

    test: check for the existence of inprocessing and outprocessing tabs

--- frontend/cypress/integration/e2e-bitesized/sherri.spec.js
+++ frontend/cypress/integration/e2e-bitesized/sherri.spec.js
@@ -6,8 +6,15 @@ describe("testing the User Dashboard View -- Inprocessing", () => {
 		cy.get("button").first().click();
 	});
 
+	it("should have tabs for Inprocessing and Outprocessing", () => {
+		cy.url().should("eq", "http://localhost:3000/dashboard");
+		cy.get("button").contains("Inprocessing Tasks").should("exist");
+		cy.get("button").contains("Outprocessing Tasks").should("exist");
+	});
+
 	it("the test User Sherri Ortiz should have 3 Inprocessing Tasks", () => {
 		cy.url().should("eq", "http://localhost:3000/dashboard");
+		cy.get("button").contains("Inprocessing Tasks").click();
 		cy.get("input").should("have.length", 3);
 	});
 
@@ -16,5 +23,13 @@ describe("testing the User Dashboard View -- Inprocessing", () => {
 		for (let i = 0; i < cy.get("input").length; i++) {
 			cy.get("input").eq(i).should("have.value", "false");
 		}
+	});
+
+	it("clicking a checkbox should mark the task complete or incomplete as needed", () => {
+		cy.url().should("eq", "http://localhost:3000/dashboard");
+		cy.get("input").first().click();
+		cy.get("input").first().should("have.value", "true");
+		cy.get("input").first().click();
+		cy.get("input").first().should("have.value", "false");
 	});
 });
