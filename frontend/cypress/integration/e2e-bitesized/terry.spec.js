d3827dcd59476355cd6beb71f3892fba80aa675b
tkbdsi <patient-remix-0q@icloud.com>
2022-05-10 12:47:57 -0400

    test: test an Admin user for tabs

---
+++ frontend/cypress/integration/e2e-bitesized/terry.spec.js
@@ -0,0 +1,19 @@
+describe("testing the User Dashboard View -- Inprocessing", () => {
+	beforeEach(() => {
+		cy.visit("http://localhost:3000/");
+		cy.get("input").first().type("terry.schiller@spaceforce.mil");
+		cy.get("input").last().type("1234567890qwertyuiop");
+		cy.get("button").first().click();
+	});
+
+	it("should have tabs for Inprocessing and Outprocessing", () => {
+		cy.url().should("eq", "http://localhost:3000/dashboard");
+		cy.get("button").contains("Inprocessing Tasks").should("exist");
+		cy.get("button").contains("Outprocessing Tasks").should("exist");
+	});
+
+	it("should have tab for modifying subordinate admins ", () => {
+		cy.url().should("eq", "http://localhost:3000/dashboard");
+		cy.get("button").contains("Modify Admins").should("exist");
+	});
+});
