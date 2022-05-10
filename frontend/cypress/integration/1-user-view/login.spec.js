b323b7678e23d0cf3a93a7724c49f18a4c6a9561
tkbdsi <patient-remix-0q@icloud.com>
2022-05-10 11:39:38 -0400

    test: test for an email input

--- frontend/cypress/integration/1-user-view/login.spec.js
+++ frontend/cypress/integration/1-user-view/login.spec.js
@@ -3,8 +3,17 @@ describe("testing the Login View", () => {
 		cy.visit("http://localhost:3000/");
 	});
 
-	it("displays two todo items by default", () => {
+	it("displays two Input Fields by default", () => {
 		// After loading the Login view, check if there are two inputs fields
 		cy.get("input").should("have.length", 2);
+
+		// The input fields should be empty
+		cy.get("input").first().should("have.value", "");
+		cy.get("input").last().should("have.value", "");
+
+		cy.get("input")
+			.first()
+			.invoke("attr", "placeholder")
+			.should("contain", "Enter Email");
 	});
 });
