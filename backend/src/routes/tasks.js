72e074013fe47f2f5bb721c40f80fd118f83f973
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-11 00:21:27 -0600

    Feat: Tasks assign properly

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -3,7 +3,7 @@ import { TaskController } from "../controllers";
 
 const router = Router();
 
-router.route("/tasks").get(TaskController.list).post(TaskController.create);
+router.route("/tasks").get(TaskController.list).post(TaskController.addTask);
 router
 	.route("/tasks/:id")
 	.get(TaskController.get)
