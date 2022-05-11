cdb6caf212879bf52f7e8e38dba3dcf272057729
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-11 01:31:24 -0600

    Feat: Initial work with firebase

---
+++ backend/src/firebase.js
@@ -0,0 +1,25 @@
+import { admin } from "firebase-admin";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const { GOOGLE_APPLICATION_CREDENTIALS } = process.env;
+const apiKey = process.env.API_KEY || "api-key";
+
+const appCreds = require(GOOGLE_APPLICATION_CREDENTIALS);
+
+const config = {
+	apiKey,
+	authDomain: "bsdi-capstone.firebaseapp.com",
+	projectId: "bsdi-capstone",
+};
+
+const credentials = {
+	credential: admin.credential.cert(appCreds)
+};
+
+const serviceAccount =
+	GOOGLE_APPLICATION_CREDENTIALS !== undefined ? credentials : config;
+
+admin.initializeApp(serviceAccount);
+
+export default admin
