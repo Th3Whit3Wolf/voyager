2266c46a0b2815462a6aad945b4dcfd4b1a37227
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-12 16:59:45 -0600

    Fix: Heroku backend

--- backend/src/db/seeds/seeders/index.js
+++ backend/src/db/seeds/seeders/index.js
@@ -1,7 +1,8 @@
-import seedRoles from "./seedRoles";
-import seedTasks from "./seedTasks";
-import seedTaskUsers from "./seedTaskUsers";
-import seedUnits from "./seedUnits";
-import seedUsers from "./seedUsers";
+/* eslint-disable import/extensions */
+import seedRoles from "./seedRoles.js";
+import seedTasks from "./seedTasks.js";
+import seedTaskUsers from "./seedTaskUsers.js";
+import seedUnits from "./seedUnits.js";
+import seedUsers from "./seedUsers.js";
 
 export { seedRoles, seedTasks, seedTaskUsers, seedUnits, seedUsers };
