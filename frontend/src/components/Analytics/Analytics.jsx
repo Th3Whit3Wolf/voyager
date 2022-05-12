4f6e7be9de9887938e5583f666da8bac407d0cc5
tkbdsi <patient-remix-0q@icloud.com>
2022-05-12 08:23:50 -0400

    style: make a side-by-side analyics dashboard layout

--- frontend/src/components/Analytics/Analytics.jsx
+++ frontend/src/components/Analytics/Analytics.jsx
@@ -1,9 +1,25 @@
+// Base Packages
 import React from "react";
+
+// Our Packages
+import styles from "./Analytics.module.css";
+
+// Third Party Packages
 
 const Analytics = () => {
 	return (
-		<section>
-			<h2>Analytics Component Render</h2>
+		<section className={styles.container}>
+			<nav>
+				<h3>Navigation</h3>
+			</nav>
+			<section className={styles.sidebyside}>
+				<sidebar className={styles.sidebar}>
+					<h3>Sidebar</h3>
+				</sidebar>
+				<article>
+					<h3>Main Article</h3>
+				</article>
+			</section>
 		</section>
 	);
 };
