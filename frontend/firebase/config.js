7247975c2544ad1eb38a1e771faf57bd9868300f
ChronoNinja <patient-remix-0q@icloud.com>
2022-05-12 22:52:00 -0400

    docs: add a lot of comments on how one could use firebase.config for authentication

--- frontend/firebase/config.js
+++ frontend/firebase/config.js
@@ -19,3 +19,26 @@ firebase.initializeApp(firebaseConfig);
 const registrarAuth = firebase.auth();
 
 export { registrarAuth };
+
+// I set up the service in this file, instead of within the app
+// so that it can be directly used. For example, it can be
+// imported into Login as import {registrarAuth} from '../../firebase/config';
+// then within Login, also import {signInWithEmailAndPassword } from "firebase/auth";
+// One can then create an auth object (think of it as Context) such as by
+// const auth = registrarAuth;
+// This can be passed to the firebase function
+// signInWithEmailAndPassword(auth, email, password)
+// Which is a promise that returns credentials.
+// an example use case would then be:
+// signInWithEmailAndPassword(auth, email, password)
+//      .then( (cred) => {
+//        setUser(cred.user) <----- note, I am using my stateful Context trick here!
+//      })
+//      .catch( error => {
+//        const errorCode = error.code;
+//        const errorMsg = error.message;
+//        throw Error(`${errorCode}: ${errorMsg}`)
+//      })
+//      .finally( () => navigate('/'));
+// Please let me know if you have questions Reader,
+// --- Tony
