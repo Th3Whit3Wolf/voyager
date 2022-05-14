61b9bda5df1e7481ccc753bb93618c4a793b47bf
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-14 15:29:00 -0600

    Fix: Test not able to access theme

--- frontend/src/components/Analytics/Analytics.test.js
+++ frontend/src/components/Analytics/Analytics.test.js
@@ -1,25 +1,30 @@
 // Import Testing Utilities
 import { render, screen } from "@testing-library/react";
-
+import getDesignTokens from "../../theme.js";
 // Import Features to be Tested
 import Analytics from "./Analytics.jsx";
 import { analyticsMockAdmin } from "./analyticsMockAdmin.js";
 
 // Third Party Wrappers
 import { MemoryRouter as Router } from "react-router-dom";
-
+import { createTheme, ThemeProvider } from "@mui/material";
 // Note that I use a setup function here instead of
 // beforeAll or beforeEach due to feedback received
 // from code hardening tools such as SonarCube on
 // previous software projects. --Tony
 
 describe("RTL unit tests for the Analytics Component", () => {
-	const setup = (user = {}) =>
-		render(
-			<Router>
-				<Analytics user={user} />
-			</Router>
+	const setup = (user = {}) => {
+		const theme = createTheme(getDesignTokens("light"));
+
+		return render(
+			<ThemeProvider theme={theme}>
+				<Router>
+					<Analytics user={user} />
+				</Router>
+			</ThemeProvider>
 		);
+	};
 
 	test("it renders a Analytics component successfully", () => {
 		setup(analyticsMockAdmin);
