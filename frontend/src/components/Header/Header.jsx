846bcc14bf2103f9d38ccf0d7d1b28f67287b377
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-13 22:48:24 -0600

    Fix: Header conforms to AstroUXDS

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,26 @@
 import React, { useContext } from "react";
-import styles from "./Header.module.css";
 import logo from "../../space-force-logo.png";
 import { UserContext } from "#context";
 
-import { Button, Grid } from "@mui/material";
+import {
+	Button,
+	Link,
+	AppBar,
+	Box,
+	Toolbar,
+	Typography,
+	useTheme,
+	Container
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 // Also need a small title shows USER or ADMIN
 
-const Header = () => {
+const NewHeader = () => {
+	const theme = useTheme();
 	const navigate = useNavigate();
 	const { user, setUser } = useContext(UserContext);
+
+	console.log("THEME:", theme);
 
 	const handleClick = () => {
 		// Do some stuff
@@ -19,44 +30,97 @@ const Header = () => {
 	};
 
 	return (
-		<header className={styles.header__container}>
-			<a
-				href="https://voyager.jpl.nasa.gov/mission/status/" // <- love it
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<img src={logo} alt="Space Force Logo" />
-			</a>
-			<div>
-				<h1 style={user?.role && { marginBottom: "-20px" }}>
-					<span className={styles.header__title2}>Voyager</span>
-				</h1>
-				{user?.role && <h4>{user.role.kind.replace("_", " ")} VIEW</h4>}
-			</div>
+		<AppBar
+			position="static"
+			enableColorOnDark
+			sx={{
+				height: 100,
+				display: "flex",
+				padding: "5px 5px 5px 25px",
+				flexGrow: 1,
+				backgroundColor: theme.palette.gsb.background
+			}}
+		>
+			<Toolbar disableGutters>
+				<Link
+					href="https://voyager.jpl.nasa.gov/mission/status/"
+					underline="none"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Box
+						disableGutters
+						component="img"
+						sx={{
+							p: 0,
+							m: 0,
+							height: 80,
+							width: 70,
+							mr: "15px"
+						}}
+						src={logo}
+						alt="Space Force Logo"
+					/>
+				</Link>
 
-			<Grid
-				className={styles.header__button}
-				sx={{
-					display: "flex",
-					flexGrow: 1,
-					justifyContent: "flex-end"
-				}}
-			>
-				{user?.firstName ? (
-					<div className={styles.header__title3}>
-						<p style={{ marginRight: "2rem" }}>Welcome, {user.firstName}!</p>{" "}
-						<Button
-							variant="contained"
-							onClick={handleClick}
-							data-testid="logoutButton"
+				<Container disableGutters>
+					<h1
+						style={{
+							color: theme.palette.gsb.text,
+							marginBottom: user?.role ? -20 : 0
+						}}
+					>
+						<span style={{ color: theme.palette.gsb.text }}>Voyager</span>
+					</h1>
+
+					{user?.role && (
+						<h6 style={{ color: theme.palette.gsb.primary }}>
+							{user.role.kind.replace("_", " ")} VIEW
+						</h6>
+					)}
+				</Container>
+
+				<Box
+					sx={{
+						display: "flex",
+						flexGrow: 1,
+						justifyContent: "flex-end"
+					}}
+				>
+					{user?.firstName ? (
+						<Container
+							disableGutters
+							sx={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "space-between",
+								color: theme.palette.gsb.text,
+								pr: "5%",
+								alignItems: "center"
+							}}
 						>
-							Logout
-						</Button>
-					</div>
-				) : null}
-			</Grid>
-		</header>
+							<Typography variant="p" sx={{ mr: "2rem" }}>
+								Welcome, {user.firstName}!
+							</Typography>
+							<Button
+								variant="contained"
+								onClick={handleClick}
+								data-testid="logoutButton"
+								sx={{
+									backgroundColor: theme.palette.gsb.primary,
+									mr: "0.75rem"
+								}}
+							>
+								Logout
+							</Button>
+						</Container>
+					) : (
+						""
+					)}
+				</Box>
+			</Toolbar>
+		</AppBar>
 	);
 };
 
-export default Header;
+export default NewHeader;
