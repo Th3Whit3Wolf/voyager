b480c3b3618246fbd3ad81b75817d2605ee58e83
tkbdsi <patient-remix-0q@icloud.com>
2022-05-14 09:15:09 -0400

    test: fix the installation admin user e2e test due to seed generation changes

--- frontend/cypress/integration/e2e-bitesized/johnny.spec.js
+++ frontend/cypress/integration/e2e-bitesized/johnny.spec.js
@@ -1,6 +1,8 @@
 describe("testing the Admin Dashboard View -- Inprocessing", () => {
 	beforeEach(() => {
 		cy.visit("http://localhost:3000/");
+		cy.get("input").first().clear();
+		cy.get("input").last().clear();
 		cy.get("input").first().type("johnny.bravo@spaceforce.mil");
 		cy.get("input").last().type("1234567890qwertyuiop");
 		cy.get("button").first().click();
@@ -17,18 +19,18 @@ describe("testing the Admin Dashboard View -- Inprocessing", () => {
 		cy.get("button").contains("Modify Admins").should("exist");
 	});
 
-	it("the test Admin Johnny Bravo should have 17 Inprocessing Tasks", () => {
+	it("the test Admin Johnny Bravo should have 3 Inprocessing Tasks", () => {
 		const numberOfColumns = 6;
 		cy.url().should("eq", "http://localhost:3000/dashboard");
 		cy.get("[data-testid=buttonInprocessingTasks]").click();
-		cy.get("input").should("have.length", 20 * numberOfColumns);
+		cy.get("input").should("have.length", 3 * numberOfColumns);
 	});
 
-	it("the test Admin Johnny Bravo should have 12 Outprocessing Tasks", () => {
+	it("the test Admin Johnny Bravo should have 6 Outprocessing Tasks", () => {
 		const numberOfColumns = 6;
 		cy.url().should("eq", "http://localhost:3000/dashboard");
 		cy.get("[data-testid=buttonOutprocessingTasks]").click();
-		cy.get("input").should("have.length", 14 * numberOfColumns);
+		cy.get("input").should("have.length", 6 * numberOfColumns);
 	});
 
 	let numberOfInprocessingRows = 0;
@@ -64,11 +66,10 @@ describe("testing the Admin Dashboard View -- Inprocessing", () => {
 			});
 	});
 
-	/*
 	it("A Series of Checks: Step 4 - There Should Be One More Row than There had Been", () => {
 		expect(numberOfInprocessingRowsNew - numberOfInprocessingRows).to.equal(1);
 	});
-	*/
+
 	it("A Series of Checks: Step 5 - Find Last Row, Get Delete Button, Click It", () => {
 		cy.url().should("eq", "http://localhost:3000/dashboard");
 		cy.get("table").find("tr").last().find("button").eq(1).click();
