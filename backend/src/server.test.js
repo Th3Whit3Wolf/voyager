c95169ebb6c16e4fd29ebf296d3ee3a4e2e00d9b
David Karrick <48275422+Th3Whit3Wolf@users.noreply.github.com>
2022-05-14 18:42:09 -0600

    Update server.test.js

--- backend/src/server.test.js
+++ backend/src/server.test.js
@@ -7,12 +7,12 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID
+	apiKey: process.env.VITE_REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.VITE_REACT_APP_FIREBASE_APP_ID
 };
 
 const { TEST_EMAIL, TEST_PASSWORD } = process.env;
