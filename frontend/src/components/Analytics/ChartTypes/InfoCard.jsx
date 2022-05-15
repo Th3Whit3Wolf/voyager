0c8184d12a7ae4858bc716232fcbc89aca97c673
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-14 23:49:10 -0600

    Style: InfoCard is multi themed

--- frontend/src/components/Analytics/ChartTypes/InfoCard.jsx
+++ frontend/src/components/Analytics/ChartTypes/InfoCard.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 
 // Third Party Packages
-import { Card } from "@mui/material";
+import { Card, CardHeader, CardContent, useTheme } from "@mui/material";
 
+const InfoCard = ({ title, value }) => {
+	const theme = useTheme();
+	return (
+		<Card
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				borderRadius: "0.75rem",
+				maxWidth: "100%",
+				boxShadow:
+					theme.palette.mode === "light"
+						? "0 .75em 1.5em -.185em rgba(	10, 10, 10,.2), 0 0 1px rgba(	10, 10, 10,.02)"
+						: "0 .75em 1.5em -.185em rgba(	0, 0, 0,.9), 0 0 1px rgba(	0, 0, 0,.02)"
+			}}
+		>
+			<CardHeader
+				title={title}
+				sx={{
+					display: "flex",
+					flexGrow: 1,
+					fontWeight: 700,
+					padding: "0.75rem 1.5rem",
+					pb: "0.1rem"
+				}}
+				titleTypographyProps={{ variant: "h3" }}
+			></CardHeader>
+			<CardContent
+				typographyProps={{ variant: "h4" }}
+				sx={{
+					padding: "1.5rem",
+					pt: "0.5rem",
+					fontSize: "1.25rem"
+				}}
+			>
+				{value}
+			</CardContent>
+		</Card>
+	);
+};
+
+/*
 const InfoCard = ({ title, value }) => {
 	return (
 		<Card
@@ -24,5 +66,6 @@ const InfoCard = ({ title, value }) => {
 		</Card>
 	);
 };
+*/
 
 export default InfoCard;
