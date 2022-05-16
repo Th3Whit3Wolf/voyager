c1d5d2ca6c09fabcb4da4db3662939ad9069147a
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-16 10:37:40 -0600

    Chore: Move back to react 17

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
 // Third Party Components
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
 	<React.StrictMode>
 		<Router>
 			<App />
 		</Router>
-	</React.StrictMode>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
