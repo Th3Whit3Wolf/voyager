f4a295d6794216cba54267bc10011ac71c5879d2
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-18 13:28:14 -0600

    Fix: Heroku backend attemp 3

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,19 +1,32 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
-const { FIREBASE_VOYAGER_SERVICE_ACCOUNT } = process.env;
+const {
+	FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+	FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+	FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+	FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+	FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+	FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+	FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+	FIREBASE_SERVICE_ACCOUNT_CLIENT_PROVIDER_X509_CERT_URL
+} = process.env;
 
-let cert;
-
-try {
-	cert = require("/app/src/config/accountService.json");
-} catch {
-	cert = JSON.parse(FIREBASE_VOYAGER_SERVICE_ACCOUNT);
-}
+const serviceAccount = {
+	type: "service_account",
+	project_id: "bsdi-capstone",
+	private_key_id: FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+	private_key: FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+	client_email: FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+	client_id: FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+	auth_uri: FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+	token_uri: FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+	auth_provider_x509_cert_url:
+		FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+	client_x509_cert_url: FIREBASE_SERVICE_ACCOUNT_CLIENT_PROVIDER_X509_CERT_URL
+};
 
 admin.initializeApp({
-	credential: admin.credential.cert(cert)
+	credential: admin.credential.cert(serviceAccount)
 });
 
 export default admin;
--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -2,10 +2,19 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
-const config = import.meta.env.VITE_REACT_APP_FIREBASE;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env
+		.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
+};
 
 // Initialize Firebase
-firebase.initializeApp(JSON.parse(config));
+firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const registrarAuth = firebase.auth();
