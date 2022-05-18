6967c2b0d50aa119d6efae36c0efaa0a44e018ba
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-18 10:48:11 -0600

    Fix: ENV issues

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -3,18 +3,22 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env
-		.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
-};
+// const firebaseConfig = {
+// 	apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+// 	authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+// 	projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+// 	storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+// 	messagingSenderId: import.meta.env
+// 		.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+// 	appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
+// };
+
+const config = import.meta.env.VITE_REACT_APP_FIREBASE;
+
+console.log(config);
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(JSON.parse(config));
 
 // Initialize Firebase Authentication and get a reference to the service
 const registrarAuth = firebase.auth();
