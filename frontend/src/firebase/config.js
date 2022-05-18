0583208a4659c8cf943d14f72e4c0ce559632f65
David Karrick <48275422+Th3Whit3Wolf@users.noreply.github.com>
2022-05-18 13:51:40 -0600

    Update config.js

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -3,15 +3,17 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env
-		.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyB_Ph-X_XyXlLN7fjyJhoiS73bZs-o-rfU",
+  authDomain: "bsdi-capstone.firebaseapp.com",
+  projectId: "bsdi-capstone",
+  storageBucket: "bsdi-capstone.appspot.com",
+  messagingSenderId: "831411631880",
+  appId: "1:831411631880:web:32bf1f0ede9a30e146debd"
 };
+
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
