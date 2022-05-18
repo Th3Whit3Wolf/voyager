daedf524087547c12ebe92e53b2bdff564cbe7f4
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-18 17:29:33 -0600

    Remove consoles

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { ViteAliases } from "vite-aliases";
 import { defineConfig } from "vite";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
 		// For more information
 		// https://github.com/subwaytime/vite-aliases
 		ViteAliases({ prefix: "#" }),
-		eslintPlugin()
+		eslintPlugin(),
+		removeConsole()
 	]
 });
