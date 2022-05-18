9d742870e0dfcf13aed523f47279043fa0a40766
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-18 13:03:17 -0600

    Fix: Heroku backend attemp 2

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -2,25 +2,18 @@ import admin from "firebase-admin";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const { FIREBASE_VOYAGER_SERVICE_ACCOUNT, IS_HEROKU } = process.env;
+const { FIREBASE_VOYAGER_SERVICE_ACCOUNT } = process.env;
+
+let cert;
+
+try {
+	cert = require("/app/src/config/accountService.json");
+} catch {
+	cert = JSON.parse(FIREBASE_VOYAGER_SERVICE_ACCOUNT);
+}
 
 admin.initializeApp({
-	credential: admin.credential.cert(
-		IS_HEROKU === undefined
-			? JSON.parse(FIREBASE_VOYAGER_SERVICE_ACCOUNT)
-			: require("/app/src/config/accountService.json")
-	)
+	credential: admin.credential.cert(cert)
 });
 
 export default admin;
-
-/*
-const { FIREBASE_VOYAGER_SERVICE_ACCOUNT } = process.env;
-
-admin.initializeApp({
-	credential: admin.credential.cert(
-		JSON.parse(FIREBASE_VOYAGER_SERVICE_ACCOUNT)
-	)
-});
-
-*/
